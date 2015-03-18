576632492eb7551c8f4aeeb121a8fbd570676ab7
PradeepPereiraGitHub <pradeep_pereira@yahoo.com>
2015-03-18 13:15:25 +0530

    Updates

--- plot3.R
+++ plot3.R
@@ -3,61 +3,27 @@ NEI <- readRDS("./exdata_data_NEI_data/summarySCC_PM25.rds")
 SCC <- readRDS("./exdata_data_NEI_data/Source_Classification_Code.rds")
 
 #Subset just the data for Baltimore
-NEIBaltimore<-NEI[NEI$fips== "24510",]
+NEIBaltimore<-NEI[NEI$fips == "24510",]
 
-NEITotalsByYearBaltimore <- aggregate(Emissions ~ year,NEIBaltimore,sum)
-deep<- aggregate(Emissions ~ year + type, NEIBaltimore, sum)
-deep1<- aggregate(Emissions ~ year, NEIBaltimore, sum)
+#ForVisual verification
+#NEITotalsByYearBaltimore <- aggregate(Emissions ~ year,NEIBaltimore,sum)
+#deep <- aggregate(Emissions ~ year + type, NEIBaltimore, sum)
+#deep1 <- aggregate(Emissions ~ year, NEIBaltimore, sum)
 
 
-#Using the base plotting system, make a plot showing the total PM2.5 emissions
-#for Baltimore for each of the years 1999, 2002, 2005, and 2008.
+#Using the ggplot2 system, make a plot showing the total PM2.5 emissions
+#for Baltimore by source type for the years 1999, 2002, 2005, and 2008.
 
 #Save the plot to a png file
 
-png("./GitHub/ExData_Plotting2/plot2.png",width=480,height=480,units="px",bg="white")
+png("./GitHub/ExData_Plotting2/plot3.png",,width=480,height=480,units="px",bg="white")
 
-qplot(x, y, data=NEIBaltimore, geom="line")
-dev.off()
-ggplot(deep,aes(factor(year),Emissions,fill=type)) + 
+myggplot <- ggplot(NEIBaltimore,aes(factor(year),Emissions,fill=type)) + 
         geom_bar(stat="identity") + 
-        theme_bw() + guides(fill=FALSE)+ 
-        facet_grid(.~type,scales = "free",space="free") +  
-        labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) +  
-        labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type")) 
-
-ggplot(data=deep, aes(factor(year),Emissions, fill=type)) +    
-        geom_bar(stat="identity", position="dodge") +
-        ggtitle("Baltimore, MD Emission by Type: 1999-2008") +
-        labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)"))  
-
-ggplot(deep,aes(type,Emissions,fill=type)) + 
-             geom_bar(stat="identity") + 
-             theme_bw() + guides(fill=FALSE)+ 
-             facet_grid(.~year,scales = "free",space="free") +  
-             labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) +  
-             labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type")) 
-
-ggplot(deep,aes(factor(year),Emissions,fill=type)) + 
-             geom_bar(stat="identity") + 
-             theme_bw() + guides(fill=FALSE)+ 
-             facet_grid(.~type,scales = "free",space="free") +  
-             labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) +  
-             labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type")) 
- 
-
-ggplot(deep,aes(factor(year),Emissions,fill=type)) + 
-        geom_bar(stat="identity") + 
-        theme_bw() +  guides(fill=FALSE) + 
+        theme_bw(base_size=14) + guides(fill=FALSE)+ 
         facet_grid(type~.,scales = "free_x",space="free") +  
-        
-           labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) +  
-           labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008")) 
+        labs(x="Year", y="Total PM2.5 Emissions (tons)") +  
+        labs(title="PM2.5 Emissions For Baltimore 1999-2008 - by Type") 
+print(myggplot)
 
-#STUPIDLY SIMPLE
-ggplot(NEIBaltimore,aes(factor(year),Emissions,fill=type)) + 
-        geom_bar(stat="identity") + 
-        theme_bw() + guides(fill=FALSE)+ 
-        facet_grid(.~type,scales = "free",space="free") +  
-        labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) +  
-        labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type")) 
+dev.off()
